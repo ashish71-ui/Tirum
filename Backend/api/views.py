a146755b8c056ceccdc95eb96637f564cc83351c
Aavash Gyawali <aavashuzumaki12@gmail.com>
2025-06-04 21:31:48 +0545

    Backend:Authentication System created

---
+++ Backend/api/views.py
@@ -0,0 +1,35 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import logout
+
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        return Response({'token': response.data['token']})
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=200)
