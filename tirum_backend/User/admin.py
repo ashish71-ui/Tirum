ca568faba09c9781771aa0b5a8146d6c280d40db
Ashish <aashishdhakal71@gmail.com>
2026-02-19 21:13:26 +0545

    User Password Hash

--- tirum_backend/User/admin.py
+++ tirum_backend/User/admin.py
@@ -1,7 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser, Group
 
-# Register your models here.
-from .models import (CustomUser, Group)
 
-admin.site.register(CustomUser)
+class CustomUserCreationForm(UserCreationForm):
+    """Custom form for creating users with properly hashed passwords"""
+    class Meta:
+        model = CustomUser
+        fields = ('username',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """Custom form for changing user details"""
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 
+                  'avatar', 'language', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    """Custom UserAdmin that properly handles password hashing"""
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'avatar', 'language')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Group)
